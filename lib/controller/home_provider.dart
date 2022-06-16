165b2ec1f4fc6d0e57f66620196ba2e6d3f7d061
MoMarwan <mohamedmarwango@gmail.com>
2022-06-16 16:12:51 +0200

    enhancment

--- lib/controller/home_provider.dart
+++ lib/controller/home_provider.dart
@@ -1,24 +1,10 @@
 import 'package:flutter/cupertino.dart';
-import 'package:movie_task/core/helper/connectivity.dart';
 import 'package:movie_task/data/models/popular_movie_model.dart';
 import 'package:movie_task/data/services/components/remote/dio_helper.dart';
 import 'package:movie_task/data/services/components/remote/url.dart';
 
 class HomeProvider extends ChangeNotifier{
   PopularMovieModel? popularMovieModel;
-
-  bool checkInternet = false ;
-  Future<void> checkInter() async {
-    if (await checkInternetConnectivity()) {
-      checkInternet =true;
-      notifyListeners();
-    }
-    else{
-      checkInternet =false;
-      notifyListeners();
-    }
-    notifyListeners();
-  }
 
   Future<PopularMovieModel?> popularMovie(int page) async {
     try{
